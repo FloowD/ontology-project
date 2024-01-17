f253d23190e9027032695801b004d6c32f6d012b
FloowD <floriandufaure@gmail.com>
2024-01-17 13:54:29 +0100

    Ajout couleur sur les nodes

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import ontologyJson from '../data/ontology.json'
 import { reactive, ref } from "vue"
 import * as vNG from "v-network-graph"
 import {
@@ -9,8 +10,6 @@ import {
 
 const NODE_COUNT = 20
 
-const nodes = reactive({})
-const edges = reactive({})
 
 // The fixed position of the node can be specified.
 const layouts = ref({
@@ -34,7 +33,7 @@ const configs = reactive(
           const forceLink = d3.forceLink<ForceNodeDatum, ForceEdgeDatum>(edges).id(d => d.id)
           return d3
             .forceSimulation(nodes)
-            .force("edge", forceLink.distance(60).strength(0.15))
+            .force("edge", forceLink.distance(60).strength(0.04))
             .force("charge", d3.forceManyBody().strength(-250))
             .alphaMin(0.001)
         }
@@ -42,33 +41,105 @@ const configs = reactive(
     },
     node: {
       normal: {
-        color: n => (n.id === "node0" ? "#ff0000" : "#4466cc"),
+        //Change the color of the node according to the level
+        color: node => node.level,
       },
       label: {
-        visible: false,
+        visible: true,
+        color: "black",
+      },
+      focusring: {
+        color: "hotpink",
       },
     },
   })
 )
 
-buildNetwork(NODE_COUNT, nodes, edges)
-
-function buildNetwork(count: number, nodes: vNG.Nodes, edges: vNG.Edges) {
-  const idNums = [...Array(count)].map((_, i) => i)
-
-  // nodes
-  const newNodes = idNums.map(id => [`node${id}`, { id: `node${id}` }])
-  Object.assign(nodes, Object.fromEntries(newNodes))
-  // edges
-  const makeEdgeEntry = (id1: number, id2: number) => {
-    return [`edge${id1}-${id2}`, { source: `node${id1}`, target: `node${id2}` }]
+function extractNodesAndEdges(dict) {
+      let nodes = {}
+      let relations = {}
+      nodes[dict.name] = {name: dict.name, level: dict.level}
+      if(dict.subs){
+        dict.subs.forEach(sub => {
+            let subnodesAndEdges = extractNodesAndEdges(sub)
+            nodes = {...nodes, ...subnodesAndEdges[0]}
+            relations = {...relations, ...subnodesAndEdges[1]}
+            relations[dict.name + sub.name] = {source: dict.name, target: sub.name}
+        })
+    }
+      return [nodes, relations]
   }
-  const newEdges = []
-  for (let i = 1; i < count; i++) {
-    newEdges.push(makeEdgeEntry(Math.floor(i / 4), i))
+
+  function changeLevelToColor(nodes){
+    let newNodes = {}
+    for (const [key, value] of Object.entries(nodes)) {
+      newNodes[key] = {name: value.name, level: levelToColor(value)}
+    }
+    return newNodes
   }
-  Object.assign(edges, Object.fromEntries(newEdges))
-}
+
+  function levelToColor(node){
+    if(node.level == 0){
+      return "red"
+    } else if(node.level == 1){
+      return "blue"
+    } else if(node.level == 2){
+      return "green"
+    } else if(node.level == 3){
+      return "yellow"
+    } else if(node.level == 4){
+      return "orange"
+    } else if(node.level == 5){
+      return "purple"
+    } else if(node.level == 6){
+      return "pink"
+    } else if(node.level == 7){
+      return "brown"
+    } else if(node.level == 8){
+      return "grey"
+    } else if(node.level == 9){
+      return "black"
+    } else {
+      return "white"
+    }
+  }
+
+  let nodesAndRelations = extractNodesAndEdges(ontologyJson)
+  
+  let nodeTest = {
+    "name": "test",
+    "level": "0",
+    "subs": [
+      {
+        "name": "sub1",
+        "level": "1",
+        "subs": [
+          {
+            "name": "subsub1",
+            "level": "2"
+          },
+          {
+            "name": "subsub2",
+            "level": "2"
+          }
+        ]
+      },
+      {
+        "name": "sub2",
+        "level": "1",
+      }
+    ]
+  }
+
+  // nodesAndRelations = extractNodesAndEdges(nodeTest)
+  nodesAndRelations[0] = changeLevelToColor(nodesAndRelations[0])
+  console.log(nodesAndRelations[0])
+  console.log(nodesAndRelations[1])
+  
+  const nodes = nodesAndRelations[0]
+  const edges = nodesAndRelations[1]
+
+
 </script>
 
 <template>
