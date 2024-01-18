27c8062691d1732dfd93b03598e83ca481c16a39
FloowD <floriandufaure@gmail.com>
2024-01-18 09:28:40 +0100

    Boutton pour afficher une partie du graph

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import ontologyJson from '../data/ontology.json'
-import { reactive, ref } from "vue"
+import { ref } from "vue"
 import * as vNG from "v-network-graph"
 import {
   ForceLayout,
@@ -10,8 +10,8 @@ import {
 
 const NODE_COUNT = 20
 
-
-// The fixed position of the node can be specified.
+const nodes = ref({});
+const edges = ref({});
 const layouts = ref({
   nodes: {
     node0: {
@@ -20,9 +20,9 @@ const layouts = ref({
       fixed: true, // Unaffected by force
     },
   },
-})
+});
 
-const configs = reactive(
+const configs = ref(
   vNG.defineConfigs({
     view: {
       layoutHandler: new ForceLayout({
@@ -41,7 +41,7 @@ const configs = reactive(
     },
     node: {
       normal: {
-        //Change the color of the node according to the level
+        // Change the color of the node according to the level
         color: node => node.level,
       },
       label: {
@@ -53,33 +53,50 @@ const configs = reactive(
       },
     },
   })
-)
+);
 
 function extractNodesAndEdges(dict) {
-      let nodes = {}
-      let relations = {}
-      nodes[dict.name] = {name: dict.name, level: dict.level}
-      if(dict.subs){
-        dict.subs.forEach(sub => {
-            let subnodesAndEdges = extractNodesAndEdges(sub)
-            nodes = {...nodes, ...subnodesAndEdges[0]}
-            relations = {...relations, ...subnodesAndEdges[1]}
-            relations[dict.name + sub.name] = {source: dict.name, target: sub.name}
-        })
-    }
-      return [nodes, relations]
+  let nodes = {};
+  let relations = {};
+  nodes[dict.name] = {name: dict.name, level: dict.level};
+  if(dict.subs){
+    dict.subs.forEach(sub => {
+      let subnodesAndEdges = extractNodesAndEdges(sub);
+      nodes = {...nodes, ...subnodesAndEdges[0]};
+      relations = {...relations, ...subnodesAndEdges[1]};
+      relations[dict.name + sub.name] = {source: dict.name, target: sub.name};
+    })
+  }
+  return [nodes, relations];
+}
+
+function extractNodesAndEdgesToLevel(dict, stopLevel) {
+  let nodes = {};
+  let relations = {};
+  nodes[dict.name] = { name: dict.name, level: dict.level };
+
+  if (dict.subs && dict.level < stopLevel) {
+    dict.subs.forEach(sub => {
+      let subnodesAndEdges = extractNodesAndEdgesToLevel(sub, stopLevel);
+      nodes = { ...nodes, ...subnodesAndEdges[0] };
+      relations = { ...relations, ...subnodesAndEdges[1] };
+      relations[dict.name + sub.name] = { source: dict.name, target: sub.name };
+    });
   }
 
-  function changeLevelToColor(nodes){
-    let newNodes = {}
-    for (const [key, value] of Object.entries(nodes)) {
-      newNodes[key] = {name: value.name, level: levelToColor(value)}
-    }
-    return newNodes
-  }
+  return [nodes, relations];
+}
 
-  function levelToColor(node){
-    if(node.level == 0){
+function changeLevelToColor(nodes){
+  let newNodes = {};
+  for (const [key, value] of Object.entries(nodes)) {
+    newNodes[key] = {name: value.name, level: levelToColor(value)};
+  }
+  return newNodes;
+}
+
+function levelToColor(node){
+  if(node.level == 0){
       return "red"
     } else if(node.level == 1){
       return "blue"
@@ -102,11 +119,18 @@ function extractNodesAndEdges(dict) {
     } else {
       return "white"
     }
-  }
+}
 
-  let nodesAndRelations = extractNodesAndEdges(ontologyJson)
-  
-  let nodeTest = {
+let nodesAndRelations = extractNodesAndEdges(ontologyJson);
+nodesAndRelations[0] = changeLevelToColor(nodesAndRelations[0]);
+console.log(nodesAndRelations[0]);
+console.log(nodesAndRelations[1]);
+
+// Initialiser les propriétés de données
+nodes.value = nodesAndRelations[0];
+edges.value = nodesAndRelations[1];
+
+let nodeTest = {
     "name": "test",
     "level": "0",
     "subs": [
@@ -131,26 +155,32 @@ function extractNodesAndEdges(dict) {
     ]
   }
 
-  // nodesAndRelations = extractNodesAndEdges(nodeTest)
-  nodesAndRelations[0] = changeLevelToColor(nodesAndRelations[0])
-  console.log(nodesAndRelations[0])
-  console.log(nodesAndRelations[1])
-  
-  const nodes = nodesAndRelations[0]
-  const edges = nodesAndRelations[1]
-
+function AfficheNiv1(){
+  //Affiche seulement le level 1 du graph
+  let nodesAndRelations = extractNodesAndEdgesToLevel(ontologyJson, 3);
+  nodesAndRelations[0] = changeLevelToColor(nodesAndRelations[0]);
+  nodes.value = nodesAndRelations[0];
+  edges.value = nodesAndRelations[1];
+  console.log(nodesAndRelations[0]);
+}
 
 </script>
 
 <template>
-  <v-network-graph
-    class="graph"
-    :zoom-level="0.5"
-    :nodes="nodes"
-    :edges="edges"
-    :layouts="layouts"
-    :configs="configs"
-  />
+  <div class="container">
+    <div class="buttons">
+      <button @click="AfficheNiv1">Affiche Niveau 1</button>
+    </div>
+
+    <v-network-graph
+      class="graph"
+      :zoom-level="0.5"
+      :nodes="nodes"
+      :edges="edges"
+      :layouts="layouts"
+      :configs="configs"
+    />
+  </div>
 </template>
 
 <style>
@@ -158,5 +188,13 @@ function extractNodesAndEdges(dict) {
   width: 1200px;
   height: 800px;
   border: 1px solid #000;
+}
+
+.container {
+  display: flex;
+}
+
+.buttons {
+  margin: 1rem;
 }
 </style>
